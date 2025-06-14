cf583990544271a7dbf915cd1c3b0f0c03ba5540
Leandro Amaya <leandroamaya360@gmail.com>
2025-06-14 12:23:18 -0600

    Actualizacion ej 3 tarea 10, terminado

--- Clase_10_(Manejo de Matrices)/10.3.1 Juego del entorno.py
+++ Clase_10_(Manejo de Matrices)/10.3.1 Juego del entorno.py
@@ -19,5 +19,46 @@ vecinas .
 
 Autor:        Leandro Amaya
 Fecha:        2025-06-14
-Estado:       [ En Proceso ]
+Estado:       [ Terminado ]
 '''
+
+N = int(input("Ingrese la cantidad de filas (N): "))
+M = int(input("Ingrese la cantidad de columnas (M): "))
+
+matriz = []
+for i in range(N):
+    while True:
+        fila = input(f"Ingrese la fila {i+1} con {M} números separados por coma: ")
+        elementos = list(map(int, fila.split(",")))
+        if len(elementos) != M:
+            print(f"Debe ingresar exactamente {M} números.")
+        else:
+            matriz.append(elementos)
+            break
+
+vecinos = []
+for i in range(N):
+    fila = []
+    for j in range(M):
+        fila.append(0)
+    vecinos.append(fila)
+
+for i in range (N):
+  for j in range(M):
+    suma=0
+    for fila in range(i-1,i+2):
+      for colum in range(j-1,j+2):
+        if 0 <= fila < N and 0 <= colum <M:
+          if fila== i and colum ==j:
+            continue
+          if matriz[fila][colum] == 1:
+            suma += 1
+    vecinos[i][j] = suma
+
+print("\nMatriz original:")
+for fila in matriz:
+    print(fila)
+
+print("\nMatriz de conteo de vecinos con valor 1:")
+for fila in vecinos:
+    print(fila)
